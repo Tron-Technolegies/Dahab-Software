fec13fc609270a75658a813e6a69c9d6537740f5
clawlynx <clawlynx77@gmail.com>
2026-04-29 17:42:41 +0530

    added new Issue reporting from dahab

---
+++ src/components/datas/MinersHistoryModal.jsx
@@ -0,0 +1,225 @@
+import React, { useEffect, useState } from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { Link } from "react-router-dom";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "90%",
+  maxHeight: 500,
+  bgcolor: "background.paper",
+  overflowY: "scroll",
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function MinersHistoryModal({
+  open,
+  handleClose,
+  history1,
+  history2,
+}) {
+  const [history, setHistory] = useState([]);
+
+  useEffect(() => {
+    const newArr = [...history1, ...history2].sort(
+      (a, b) => new Date(b.createdAt) - new Date(a.createdAt),
+    );
+    setHistory(newArr);
+  }, [history1, history2]);
+
+  console.log(history);
+
+  return (
+    <Modal
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>
+        <Typography id="modal-modal-title" variant="h6" component="h2">
+          Repair and Change History
+        </Typography>
+        <TableContainer component={Paper} sx={{ marginTop: 3 }}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow sx={{ backgroundColor: "#F9FAFB" }}>
+                <TableCell
+                  sx={{
+                    textAlign: "center",
+                    fontWeight: "bold",
+                  }}
+                >
+                  SI No
+                </TableCell>
+                <TableCell
+                  sx={{
+                    textAlign: "center",
+                    fontWeight: "bold",
+                  }}
+                >
+                  Ticket Id
+                </TableCell>
+                <TableCell
+                  sx={{
+                    textAlign: "center",
+                    fontWeight: "bold",
+                  }}
+                >
+                  Type
+                </TableCell>
+                <TableCell
+                  sx={{
+                    textAlign: "center",
+                    fontWeight: "bold",
+                  }}
+                >
+                  Issue / Change Request
+                </TableCell>
+                <TableCell
+                  sx={{
+                    textAlign: "center",
+                    fontWeight: "bold",
+                  }}
+                >
+                  Status
+                </TableCell>
+                <TableCell
+                  sx={{
+                    textAlign: "center",
+                    fontWeight: "bold",
+                  }}
+                >
+                  Opened Date
+                </TableCell>
+                <TableCell
+                  sx={{
+                    textAlign: "center",
+                    fontWeight: "bold",
+                  }}
+                >
+                  Closed Date
+                </TableCell>
+                {/* <TableCell
+                  sx={{
+                    textAlign: "center",
+                    fontWeight: "bold",
+                  }}
+                >
+                  Action
+                </TableCell> */}
+              </TableRow>
+            </TableHead>
+            <TableBody sx={{ background: "#eff6ff" }}>
+              {history?.map((item, index) => {
+                return (
+                  <TableRow
+                    key={item._id}
+                    sx={{
+                      "&:last-child td, &:last-child th": { border: 0 },
+                    }}
+                  >
+                    <TableCell
+                      component="th"
+                      scope="row"
+                      sx={{ textAlign: "center" }}
+                    >
+                      {index + 1}
+                    </TableCell>
+                    <TableCell
+                      component="th"
+                      scope="row"
+                      sx={{ textAlign: "center" }}
+                    >
+                      {item._id}
+                    </TableCell>
+                    <TableCell
+                      component="th"
+                      scope="row"
+                      sx={{ textAlign: "center" }}
+                    >
+                      {item.type === "repair" ? "Issue" : "Pool Change"}
+                    </TableCell>
+                    <TableCell
+                      component="th"
+                      scope="row"
+                      sx={{ textAlign: "center" }}
+                    >
+                      {item.type === "repair" ? (
+                        <div>{item.issueName || item.description}</div>
+                      ) : (
+                        <div>
+                          <p>Requested Pool : {item.changeRequest?.pool}</p>
+                          <p>Requested Worker : {item.changeRequest?.worker}</p>
+                        </div>
+                      )}
+                    </TableCell>
+                    <TableCell
+                      component="th"
+                      scope="row"
+                      sx={{ textAlign: "center" }}
+                    >
+                      {item.status}
+                    </TableCell>
+                    <TableCell
+                      component="th"
+                      scope="row"
+                      sx={{ textAlign: "center" }}
+                    >
+                      {new Date(item.createdAt)?.toLocaleDateString("en-US", {
+                        hour: "2-digit",
+                        minute: "2-digit",
+                        hour12: false,
+                      })}
+                    </TableCell>
+                    <TableCell
+                      component="th"
+                      scope="row"
+                      sx={{ textAlign: "center" }}
+                    >
+                      {item.resolvedOn
+                        ? new Date(item.resolvedOn)?.toLocaleDateString(
+                            "en-US",
+                            {
+                              hour: "2-digit",
+                              minute: "2-digit",
+                              hour12: false,
+                            },
+                          )
+                        : "N/A"}
+                    </TableCell>
+                    {/* <TableCell
+                      component="th"
+                      scope="row"
+                      sx={{ textAlign: "center" }}
+                    >
+                      <Link
+                        to={`/miners/miner-issue/${item._id}`}
+                        className="p-2 bg-gray-600 text-white rounded-md"
+                      >
+                        Details
+                      </Link>
+                    </TableCell> */}
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
+    </Modal>
+  );
+}
